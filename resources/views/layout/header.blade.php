339c8491844f32c513b7cb5fc96679a7e6a83921
adcident12 <santikon12@gmail.com>
2018-03-24 16:06:45 +0700

    new

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>SB Admin - Start Bootstrap Template</title>
+  <!-- Bootstrap core CSS-->
+  <!-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <!-- <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Page level plugin CSS-->
+  <!-- <link href="vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet"> -->
+  <!-- Custom styles for this template-->
+  <!-- <link href="css/sb-admin.css" rel="stylesheet"> -->
+  {{Html::style('vendor/bootstrap/css/bootstrap.min.css')}}
+  {{Html::style('vendor/font-awesome/css/font-awesome.min.css')}}
+  {{Html::style('vendor/datatables/dataTables.bootstrap4.css')}}
+  {{Html::style('css/sb-admin.css')}}
+</head>
+
+<body class="fixed-nav sticky-footer bg-dark" id="page-top">
+@include('layout.nav')
